87e7065c90bc118e9b58685706d4d7ba3442d032
DharshiniB176 <dharshinib1706@gmail.com>
2025-12-27 21:23:45 +0530

    Temp dashboard UI hard coded

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 import { routes } from './app.routes';
@@ -9,6 +10,7 @@ import { authInterceptor } from './core/interceptors/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimations(),
     provideBrowserGlobalErrorListeners(),
    provideHttpClient(
       withInterceptors([authInterceptor])
--- Frontend/src/app/components/dashboard/dashboard.ts
+++ Frontend/src/app/components/dashboard/dashboard.ts
@@ -1,16 +1,33 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { AuthService } from '../../core/services/auth-service';
-import { SkillService } from '../../core/services/skill-service';
-import { SkillSelect } from "../skill-select/skill-select";
+import { DatePicker } from 'primeng/datepicker';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-dashboard',
-  imports: [SkillSelect],
+  imports: [DatePicker, FormsModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
 export class Dashboard {
 
   auth = inject(AuthService);
+
+   // Overall progress %
+  overallProgress = 68;
+
+  // Domain-wise stats
+  domainStats = [
+    { name: 'Web Development', percent: 75, skills: ['HTML', 'CSS', 'Angular', 'Node.js'] },
+    { name: 'Data Science', percent: 52, skills: ['Python', 'SQL', 'Pandas'] },
+    { name: 'Design', percent: 34, skills: ['UI Design', 'Figma'] },
+    { name: 'Business', percent: 20, skills: ['SEO', 'Marketing'] }
+  ];
+
+  selectedDate = new Date();
+  streak = 15;
+  activeDays = 14
+  progress = 50
 }
 
